e620b43c43aed9786f9be6b45ae079892003415b
jorgengj <jorgen.jarva@hotmail.com>
2022-12-05 14:39:31 +0200

    add week 4 code

---
+++ RandomNumber/RandomNumber.cs
@@ -0,0 +1,14 @@
+﻿Random rnd = new Random();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 10; i++)
+{
+    int myRandumNum = rnd.Next(0, 11); //geneeritakse väärtusi 0 - 10
+    randomSum = randomSum+ myRandumNum;
+    Console.WriteLine($"My random number is: {myRandumNum}");
+}
+
+Console.WriteLine($"Random sum total is: {randomSum}");
